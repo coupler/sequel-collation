6d9341d9eef03bca03cb3c986ae26109806dd41d
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-11-14 14:56:48 -0600

    Don't use Sequel.tsk_require

--- lib/sequel/extensions/collation.rb
+++ lib/sequel/extensions/collation.rb
@@ -12,7 +12,7 @@ module Sequel
       unless ADAPTER_MAP.has_key?(db_type)
         # attempt to load the adapter file
         begin
-          Sequel.tsk_require("sequel/extensions/collation/adapters/#{db_type}")
+          require("sequel/extensions/collation/adapters/#{db_type}")
         rescue LoadError => e
           if !@@suppress_warnings
             warn "NOTE: Sequel::Collation does not support the database type '#{db_type}'"
